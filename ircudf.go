1d148e283ca606b737dd4d6d8a06fe12a49f86dd
hirsch <hirsch@blinkenshell.org>
2014-07-23 18:39:21 +0200

    comment

--- ircudf.go
+++ ircudf.go
@@ -1,6 +1,6 @@
-/* Package ircudf implements the Internet Realy Chat (IRC) protocol 
-as defined in rfc1459 and provides basic IRC functions
-Website: http://tools.ietf.org/html/rfc1459.html*/
+// Package ircudf implements the Internet Realy Chat (IRC) protocol 
+// as defined in rfc1459 and provides basic IRC functions
+// Website: http://tools.ietf.org/html/rfc1459.html
 package ircudf
 
 import (
@@ -24,8 +24,8 @@ type Server struct {
 }
 
 
-/* Create sets the server address and port. Example:
-freenode := ircudf.Connect("irc.freenode.org:6667") */
+// Create sets the server address and port. Example:
+// freenode := ircudf.Connect("irc.freenode.org:6667")
 func Create(addr string, nickname string) Server {
 	ref := Server{}
 	ref.server = addr
@@ -35,8 +35,8 @@ func Create(addr string, nickname string) Server {
 	return ref
 }
 
-/* Connect establishes a connection to the server.
-If no parameter is given the default timeout (5 sec) will be used. */
+// Connect establishes a connection to the server.
+// If no parameter is given the default timeout (5 sec) will be used.
 func (sock *Server) Connect(timeout ...int) {
 	wait := 5 * time.Second
 	if len(timeout) >= 1 {
@@ -49,7 +49,7 @@ func (sock *Server) Connect(timeout ...int) {
 	debug("Connect:", sock.server, "\n")
 }
 
-/* Receive receives new messages from the server and forwards them to parse(). */
+// Receive receives new messages from the server and forwards them to parse().
 func (sock *Server) Receive() {
 
 	go func() {
@@ -73,7 +73,7 @@ func (sock *Server) Receive() {
 }
 
 
-/* parse parses incoming messages from the server and triggers predefined events.*/
+// parse parses incoming messages from the server and triggers predefined events.
 func (sock *Server) parse(line string) {
 	split := strings.SplitN(line, " ", 4) 
 	split = append(split, make([]string, 4-len(split), 4-len(split))...)
@@ -88,47 +88,47 @@ func (sock *Server) parse(line string) {
 	}
 }
 
-/* nick sets or changes the nickname. The IRC server might reply an errorcode,
-if the requsted nickname is not valid or in use. */
+// nick sets or changes the nickname. The IRC server might reply an errorcode,
+// if the requsted nickname is not valid or in use. 
 func (sock *Server) nick(nickname string) {
 	sock.nickname = nickname
 	sock.send("NICK " + nickname)
 }
 
-/* user specifies the userdata at the beginning of a new connection. 
-Servername and hostname are likely to be ignored by the IRC server.
-Scheme: (Nickname!Username@Hostname): Real Name*/
+// user specifies the userdata at the beginning of a new connection. 
+// Servername and hostname are likely to be ignored by the IRC server.
+// Scheme: (Nickname!Username@Hostname): Real Name
 func (sock *Server) user(username, hostname, servername, realname string) {
 	sock.send("USER " + username + " " + hostname + " " + servername + " :" + realname)
 }
 
-/* join joins the specified channel(s). Multiple channels need to be
-seperated by a colon. */
+// join joins the specified channel(s). Multiple channels need to be
+// seperated by a colon. */
 func (sock *Server) join(channel string) {
 	sock.send("JOIN " + channel )
 }
 
-/* pong answers a ping request with a previously received reply string. */
+// pong answers a ping request with a previously received reply string.
 func (sock *Server) pong(reply string) {
 	sock.send("PONG " + reply)
 }
 
-/* privmsg sends a private message to a user or a channel.
-The parameter user can also contain multiple receivers seperated by a colon. */
+// privmsg sends a private message to a user or a channel.
+// The parameter user can also contain multiple receivers seperated by a colon.
 func (sock *Server) privmsg(user string, message string, ) {
 	sock.send("PRIVMSG " + user + " :" + message)
 }
 
  
-/* send adds the message to the sendqueue (similar to a FIFO stack) */
+// send adds the message to the sendqueue (similar to a FIFO stack)
 func (sock *Server) send(message string) {
 	go func() {
 		sock.sendqueue <- message + "\n"
 	}()
 }
 
-/* sendroutine processes the sendqueue and sends the messages to the server.
-sendrouting must be executed as goroutine!*/ 
+// sendroutine processes the sendqueue and sends the messages to the server.
+// sendrouting must be executed as goroutine!
 func (sock *Server) sendroutine() {
 	for {
 		smsg := <-sock.sendqueue
@@ -137,7 +137,7 @@ func (sock *Server) sendroutine() {
 	}
 }
 
-/* DEBUG FUNCTIONS */
+// DEBUG FUNCTIONS
 
 func errcheck(err error) {
 	if err != nil {
